6de256c33ad99abcd7d8565ae5df78f5206197f8
Peiyang Wang <plukewang@gmail.com>
2024-03-08 09:11:46 -0600

    update to add test get call for API

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));//parses user requests
 
-
+app.get("/",(req,res)=>{
+    res.json({
+        id: 1,
+        name: "Hello!",
+    });
+})
 
 app.listen(port, function(err){
     if (err) console.log("Error in server setup")
