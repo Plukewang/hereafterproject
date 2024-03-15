f50ebe530b562b17da848a8ceb7b310447bad3d4
Peiyang Wang <plukewang@gmail.com>
2024-03-15 11:11:01 -0500

    Add delete route and edit route

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import Player from './player-side/player.js';
 import { createPlayerStats } from './player-side/player.js';
+import pg from "pg"
 
 const app = express();
 const port = 8080;
+const db = new pg.Client({
+    host: "localhost",
+    user: "postgres",
+    database: "hereafter",
+    password: "dxy0430BIOON",
+    port: "5432",
+})
+
+await db.connect();
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));//parses user requests
@@ -26,6 +37,56 @@ app.get("/player/check",async (req,res)=>{
 
     try{
         res.json(Joe.playerStatCheck("PHY"));
+    }catch(err){
+        console.error(err);
+    }
+})
+
+app.get("/blog",async (req,res)=>{
+    //test api json code TODO: link to player database
+    
+
+    try{
+
+        let result = await db.query('SELECT * FROM blog ORDER BY id DESC');
+        let posts = result.rows;
+
+        res.json(posts);
+
+    }catch(err){
+        console.error(err);
+    }
+})
+
+app.post("/blog/post", async (req,res)=>{
+    try{
+        
+        let result = await db.query('INSERT INTO blog (title, post) VALUES ($1, $2);', 
+        [req.body.title, req.body.post]
+        );
+        
+    }catch(err){
+        console.error(err);
+    }
+});
+
+app.post("/blog/delete", async (req,res)=>{
+    try{
+        
+        let result = await db.query('DELETE FROM blog WHERE id = ($1);', 
+        [req.body.id]
+        );
+        
+    }catch(err){
+        console.error(err);
+    }
+});
+
+app.patch("/blog/edit", async(req,res)=>{
+    try{
+        let result = await db.query('UPDATE blog SET title = ($1), post = ($2) WHERE id = ($3)',
+        [req.body.title, req.body.post, req.body.id] );
+        res.end();
     }catch(err){
         console.error(err);
     }
